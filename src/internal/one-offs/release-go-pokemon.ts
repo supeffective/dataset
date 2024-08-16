3937766cb513adedb1e7994dae74beb078ec53db
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-08-16 12:58:29 +0200

    data: release all Pokemon for GO except newest mythicals and legendaries, or game-locked forms

---
+++ src/internal/one-offs/release-go-pokemon.ts
@@ -0,0 +1,35 @@
+import type { Pokemon, PokemonIndexItem } from '../..'
+import { getDataPath, readFileAsJson, writeFile } from '../utils/fs'
+
+function runUpdateIndexFile() {
+  // Get the data file contents
+  const dataFile = getDataPath('pokemon-index.json')
+  const records = readFileAsJson<PokemonIndexItem[]>(dataFile)
+
+  for (const record of records) {
+    const pokeFile = getDataPath(`pokemon/${record.id}.json`)
+    const srcPoke = readFileAsJson<Pokemon>(pokeFile)
+    const isGenGte7Mythical = srcPoke.generation >= 7 && srcPoke.isMythical
+    const isGenGte8Legend = srcPoke.generation >= 8 && (srcPoke.isLegendary || srcPoke.isMythical)
+
+    if (srcPoke.generation === 8 || isGenGte7Mythical || isGenGte8Legend) {
+      continue
+    }
+
+    if (!srcPoke.obtainableIn.includes('go')) {
+      srcPoke.obtainableIn.push('go')
+    }
+
+    if (srcPoke.obtainableIn.includes('go') && !srcPoke.storableIn.includes('go')) {
+      srcPoke.storableIn.push('go')
+    }
+
+    if (srcPoke.obtainableIn.includes('go') && !srcPoke.registrableIn.includes('go')) {
+      srcPoke.registrableIn.push('go')
+    }
+
+    writeFile(pokeFile, JSON.stringify(srcPoke, null, 2))
+  }
+}
+
+runUpdateIndexFile()
